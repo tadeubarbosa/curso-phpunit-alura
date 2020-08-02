b5cd66d6ac600024f14c3d05cb160a211fbd395a
Tadeu Barbosa <tadeufbarbosa@gmail.com>
2020-08-02 17:24:11 -0300

    cria teste para Leilao

--- tests/Model/LeilaoTest.php
+++ tests/Model/LeilaoTest.php
@@ -9,18 +9,40 @@ use PHPUnit\Framework\TestCase;
 
 class LeilaoTest extends TestCase
 {
-  public function testLeilaoDeveReceberLances()
+  /**
+   * @dataProvider geraLances
+   * @param integer $qtdLances
+   * @param Leilao $leilao
+   * @param array $valores
+   * @return void
+   */
+  public function testLeilaoDeveReceberLances(
+    int $qtdLances,
+    Leilao $leilao,
+    array $valores
+  ) {
+    static::assertCount($qtdLances, $leilao->getLances());
+
+    foreach ($valores as $index => $valorEsperado) {
+      static::assertEquals($valorEsperado, $leilao->getLances()[$index]->getValor());
+    }
+  }
+
+  public function geraLances()
   {
     $joao = new Usuario('Joao');
     $maria = new Usuario('Maria');
 
-    $leilao = new Leilao('Fiat 147');
+    $leilaoComDoisLances = new Leilao('Fiat 147');
+    $leilaoComDoisLances->recebeLance(new Lance($joao, 1000));
+    $leilaoComDoisLances->recebeLance(new Lance($maria, 2000));
 
-    $leilao->recebeLance(new Lance($joao, 1000));
-    $leilao->recebeLance(new Lance($maria, 2000));
+    $leilaoComUmLance = new Leilao('Fusca 1972');
+    $leilaoComUmLance->recebeLance(new Lance($maria, 5000));
 
-    static::assertCount(2, $leilao->getLances());
-    static::assertEquals(1000, $leilao->getLances()[0]->getValor());
-    static::assertEquals(2000, $leilao->getLances()[1]->getValor());
+    return [
+      '2-lances' => [2, $leilaoComDoisLances, [1000, 2000]],
+      '1-lance' => [1, $leilaoComUmLance,    [5000]]
+    ];
   }
 }
